163d6b66955454a2e2a5721ba477339e9bcde9e6
Shubham Sharma <lordagentx@gmail.com>
2023-11-10 15:46:43 +0530

    Minor fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import r2wc from '@r2wc/react-to-web-component';
 import App from './App';
 
-const lastbotWidget = r2wc(App, { props: { baseUrl: "string" } });
+const lastbotWidget = r2wc(App, { props: { baseUrl: "string" }, shadow: 'open' });
 
 customElements.define("lastbot-widget", lastbotWidget);
